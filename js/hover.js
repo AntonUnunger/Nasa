8ed34cc2319f8c5c908b4b34ced413a1d3b3c167
th67472 <tobbe_hson@hotmail.com>
2018-09-19 18:01:36 +0200

    Working fade on newsbox

--- js/hover.js
+++ js/hover.js
@@ -1,11 +1,40 @@
 
-function MyLeave()
+function FadeIn()
 {
-    document.getElementById("news-info1").style.display = "none";
+    document.getElementById('news-info1').style.opacity = (parseFloat(document.getElementById('news-info1').style.opacity) + 0.1);
+    if (document.getElementById('news-info1').style.opacity < 1) {
+        setTimeout(FadeIn, 50);
+    }
+    else{document.getElementById("news-info1").style.opacity = "1";}
 }
-
+function FadeOut()
+{
+    document.getElementById('news-info1').style.opacity = (parseFloat(document.getElementById('news-info1').style.opacity) - 0.1);
+    if (document.getElementById('news-info1').style.opacity > 0) {
+        setTimeout(FadeOut, 50);
+    }else{
+    document.getElementById("news-info1").style.display = "none";
+    }
+}
+function MyLeave()
+{ 
+    if (document.getElementById('news-info1').style.opacity < 1) {
+        setTimeout(MyLeave, 100);
+    }
+    else
+    {
+        setTimeout(FadeOut, 50);
+    }
+}
 function MyHover()
 {
-    document.getElementById("news-info1").style.display = "block";
-    document.getElementById("news-info1").style.position = "absolute";
+    if (document.getElementById('news-info1').style.opacity > 0) {
+        setTimeout(MyHover, 100);
+    }
+    else
+    {
+        document.getElementById("news-info1").style.opacity = "0";
+        document.getElementById("news-info1").style.display = "block";
+        setTimeout(FadeIn, 50);
+    }  
 }
