51954785023fb9f98016970fb59461bb68296fa8
th67472 <tobbe_hson@hotmail.com>
2018-09-19 13:13:42 +0200

    JS to change hover effect on newsbox

---
+++ js/hover.js
@@ -0,0 +1,11 @@
+
+function MyLeave()
+{
+    document.getElementById("news-info1").style.display = "none";
+}
+
+function MyHover()
+{
+    document.getElementById("news-info1").style.display = "block";
+    document.getElementById("news-info1").style.position = "absolute";
+}
